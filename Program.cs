6f33ece7d1801718b734dc5e93970d2b95ae6f57
sumzali083 <alisummer066@icloud.com>
2026-05-19 21:02:07 +0100

    Read and display OrderIds from orders.csv
    Updated Program.cs to read and print OrderIds from the new orders.csv file, and display the total number of orders. Added orders.csv with sample order data for testing and demonstration. Removed the default "Hello, World!" output.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+string[] text = File.ReadAllLines("orders.csv");
+
+//Console.WriteLine(text);
+
+foreach (string x in text)
+{
+    string[] columns = x.Split(",");
+    Console.WriteLine(columns[0]);
+    
+}
+
+int max = text.Length - 1;
+Console.WriteLine("number of orders = "+max);
