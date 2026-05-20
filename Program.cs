034c6c36d0117e4e53b0025f45d0bfbc9f003282
sumzali083 <alisummer066@icloud.com>
2026-05-20 21:52:39 +0100

    Read CSV from absolute path and sum delivered revenue
    Changed file reading to use an absolute path. Added logic to calculate and print the total revenue for orders with a "Delivered" status by summing values from the appropriate column.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.Data.Common;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-string[] text = File.ReadAllLines("orders.csv");
+string[] text = File.ReadAllLines(@"C:\Users\summe\source\repos\sumzali083\orders-csharp\orders.csv");
 
 //Console.WriteLine(text);
-
+double totalRevenue = 0;
+double 
 foreach (string x in text)
 {
     string[] columns = x.Split(",");
     Console.WriteLine(columns[0]);
-    
+    if (columns[10] == "Delivered") {
+        double revenue = double.Parse(columns[9]);
+        totalRevenue += revenue;
+    }
 }
 
 int max = text.Length - 1;
 Console.WriteLine("number of orders = "+max);
+Console.WriteLine(totalRevenue);
